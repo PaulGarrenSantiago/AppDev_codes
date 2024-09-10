adeaa7d6d10f91dc5d65583a5a6c6a8073e3f2d9
Paul Garren Santiago <153277226+PaulGarrenSantiago@users.noreply.github.com>
2024-09-10 20:21:25 +0800

    First bootstrap Activity
    Pinagpraktisan lang

---
+++ activity1.php
@@ -0,0 +1,184 @@
+<html>
+    <head>
+        <title>ITEL-2</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <script src="js/jquery.js"></script>
+        <script src="js/bootstrap.js"></script>
+    
+    </head>
+        <body>
+            <h1>Activity 1</h1>
+
+            <div class="container-fluid">
+                <table class="table">
+                    <tr class="danger">
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Remarks</th>
+                    </tr>
+                    <tr>
+                        <td>001</td>
+                        <td>Alex</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>002</td>
+                        <td>Chris</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>003</td>
+                        <td>Jack</td>
+                        <td class="danger">Failed</td>
+                    </tr>
+
+                </table>
+                <table class="table table-striped">
+                    <tr class="info">
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Remarks</th>
+                    </tr>
+                    <tr>
+                        <td>001</td>
+                        <td>Alex</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>002</td>
+                        <td>Chris</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>003</td>
+                        <td>Jack</td>
+                        <td class="danger">Failed</td>
+                    </tr>
+                </table>
+                <table class="table table-condensed">
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Remarks</th>
+                    </tr>
+                    <tr>
+                        <td>001</td>
+                        <td>Alex</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>002</td>
+                        <td>Chris</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>003</td>
+                        <td>Jack</td>
+                        <td>Failed</td>
+                    </tr>
+                </table>
+                <table class="table table-hover table-bordered">
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Remarks</th>
+                    </tr>
+                    <tr>
+                        <td>001</td>
+                        <td>Alex</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>002</td>
+                        <td>Chris</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>003</td>
+                        <td>Jack</td>
+                        <td>Failed</td>
+                    </tr>
+                </table>    
+                <table class="table table-bordered">
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Remarks</th>
+                    </tr>
+                    <tr>
+                        <td>001</td>
+                        <td>Alex</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>002</td>
+                        <td>Chris</td>
+                        <td>Passed</td>
+                    </tr>
+                    <tr>
+                        <td>003</td>
+                        <td>Jack</td>
+                        <td>Failed</td>
+                    </tr>
+                </table>
+            </div>
+
+
+            <hr/>
+            <br/>
+
+            <div class="container">
+                <p class="text-success bg-success">Success</p>
+                <p class="text-danger bg-danger">Danger</p>
+                <p class="text-info bg-info">Info</p>
+                <p class="text-primary bg-primary">Primary</p>
+                <p class="text-muted bg-muted">Muted</p>
+                <p class="text-warning bg-warning">Warning</p>
+
+                <p class="lead">Information Technology</p>
+                <p class="small">Information Technology</p>
+                <p class="text-center">Information Technology</p>
+                <p class="text-right">Information Technology</p>
+                <p class="text-uppercase">Information technology</p>
+                <p class="text-lowercase">Information Technology</p>
+            </div>
+
+            <hr/>
+            <br/>
+
+            <div class = "container-fluid">
+                <div class = "row">
+                    <div class="col-md-1" style = "background: green">grid</div>
+                    <div class="col-md-1" style = "background: red">grid</div>
+                    <div class="col-md-5" style = "background: blue">grid</div>
+                </div>
+
+                <div class = "row">
+                    <div class="col-md-6" style = "background: orange">grid</div>
+                </div>
+            </div>
+
+            <hr/>
+            <br/>
+
+            <div class = "container-fluid">
+                <header>
+                    <h1 class ="jumbotron">Container fluid</h1>
+                </header>
+            </div>
+
+            <div class ="container">
+                <header>
+                    <h1 class ="jumbotron">Container fluid</h1>
+                </header>
+            </div>
+
+
+            <hr/>
+            <hr/>
+            <div class ="jumbotron">
+                <h1 class ="jumbotron">hello</h1>
+            </div>      
+        </body> 
+        </center>
+</html> 
