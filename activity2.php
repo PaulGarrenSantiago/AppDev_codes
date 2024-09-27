085a21ca575a4d1a8f8b10ca1ead803ec74595a4
Paul Garren <santiago.paul.garren@gmail.com>
2024-09-27 10:16:20 +0800

    activity 2 done

--- activity2.php
+++ activity2.php
@@ -3,12 +3,65 @@
         <title>ITEL-2</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <script src="js/jquery.js"></script>
-        <script src="js/bootstrap.js"></script>
+        <script src="js\bootstrap.min.js"></script>
+        
     
     </head>
         <body>
             <h1>Activity 2</h1>
-            
+            <div class="container-fluid">
+                <a href="#">Messages <span class="badge">10</span></a><br/>
+                <a href="#">Notifications <span class="badge">10</span></a><br/>
+                <button class="btn btn-primary">Click to see notifications <span class="badge">500</span></button>
+                <div class="page-header">
+                    <h3>Downloads <span class="label label-default">Version 10.1</span></h3>
+                </div>
+
+            </div>
+
+
+            <hr>
+            <br/>
+            <br/>
+            <div class="container-fluid">
+                <div class="alert alert-danger fade in"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already exists!!!</div>
+                <div class="alert alert-success"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already exists!!!</div>
+                <div class="alert alert-warning"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already exists!!!</div>
+                <div class="alert alert-info"><a href="#" class="close" data-dismiss="alert">&times;</a>Record already exists!!!</div>
+            </div>
+
+            <hr>
+            <br/>
+            <br/>
+            <div class="container-fluid">
+                <div class="jumbotron">
+                    <h1>Heading in a jumbotron</h1>
+                </div>
+                <h1>Heading without jumbotron</h1>
+                <div class="page-header">
+                    <h1>Welcome</h1>
+                </div>
+
+                <p class="well well-sm">This is the well</p>
+                <p class="well">This is the well</p>
+                <p class="well well-lg">This is the well</p>
+
+            </div>
+
+
+            <hr>
+            <br/>
+            <br/>
+            <div class="container-fluid">
+                <span class="glyphicon glyphicon-trash"></span>:Trash can<br/>
+                <span class="glyphicon glyphicon-pencil"></span>:Pencil<br/>
+                <button class="btn btn-success"><span class="glyphicon glyphicon-search"></span> find</button>
+
+            </div>
+
+            <hr>
+            <br/>
+            <br/>
             <div class="container-fluid">
                 <div class="dropdown">
                     <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown">College<span class="caret"></span></button>
